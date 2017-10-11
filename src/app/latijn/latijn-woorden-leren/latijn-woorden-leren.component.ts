32fa2311dc4c2152f6ab063178052b86213f149c
vdab-pierre @desktop <pierre.colin@vdab.be>
2017-10-11 15:38:22 +0200

    form probeersels

--- src/app/latijn/latijn-woorden-leren/latijn-woorden-leren.component.ts
+++ src/app/latijn/latijn-woorden-leren/latijn-woorden-leren.component.ts
@@ -14,15 +14,18 @@ import { LatijnService } from '../../latijn.service'
   styleUrls: ['./latijn-woorden-leren.component.css']
 })
 export class LatijnWoordenLerenComponent implements OnInit {
-
+  aantGeg: number;
+  aantFout: number;
   alleWoorden: any[];
   selectedWoorden = [];
   selectedWoordVan = null;
   selectedWoordTot = null;
   woordenVan: any[];
+  getypt:any[];
 
   constructor(private _latijnService: LatijnService,
-    private _location: Location) { }
+    private _location: Location) {
+  }
 
   ngOnInit() {
     this._latijnService.getAllWoorden().then((w: any) => this.alleWoorden = w);
@@ -38,25 +41,68 @@ export class LatijnWoordenLerenComponent implements OnInit {
 
   leren: boolean = false;
   huidigWoordIndex = 0;
+  btnStartEnabled = true;
 
   btnStart(): void {
     this.selectWoorden();
+    this.getypt=this.selectedWoorden.slice();
     this.leren = true;
     this.huidigWoordIndex = 0;
+    this.btnStartEnabled = false;
   }
   buttonNextText: string = "Next";
   toonResultaat = false;
-  nextWoord(el): void {
+  nextWoord(form): void {
     //nog iets voorzien voor als op het einde van de reeks zijn
-    if ((this.huidigWoordIndex + 1) < this.selectedWoorden.length) {
+    if ((this.huidigWoordIndex + 2) === this.selectedWoorden.length) {
+      //voorlaatste
       this.huidigWoordIndex++;
-      console.log(el);
-    } else {
-      //console.log(`je hebt ${this.aantGeg-this.aantFout} juist ingevuld van de ${this.aantGeg} gegevens.`);
       this.buttonNextText = "Klaar!";
+      console.log(this.huidigWoordIndex);
+    } else if (this.huidigWoordIndex + 1 === this.selectedWoorden.length) {
+      //laatste
       this.leren = false;
       this.toonResultaat = true;
-    };
+      console.log(this.huidigWoordIndex);
+      console.log(form);
+    } else if (this.huidigWoordIndex + 1 < this.selectedWoorden.length) {
+      //anderen id reeks
+      console.log(`je hebt ${this.aantGeg - this.aantFout} juist ingevuld van de ${this.aantGeg} gegevens.`);
+      this.huidigWoordIndex++;
+      console.log(this.huidigWoordIndex);
+    }
+  }
+
+  checkGenus(genus: string, el: any): void {
+    if (genus != "" && genus != this.selectedWoorden[this.huidigWoordIndex].genus) {
+      //el.focus();
+      el.select();  //el.setSelectionRange(0,el.value.length); in sommige browsers 
+      if (!el.classList.contains("fout")) el.classList.add("fout");
+      this.aantFout += 1;
+    } else {
+      if (el.classList.contains("fout")) el.classList.remove("fout");
+    }
+  }
+  checkVert(index: number, vert: string, el: any): void {
+    if (vert != "" && vert != this.selectedWoorden[this.huidigWoordIndex].vert[index].term) {
+      //el.focus();
+      el.select();  //el.setSelectionRange(0,el.value.length); in sommige browsers 
+      if (!el.classList.contains("fout")) el.classList.add("fout");
+      this.aantFout += 1;
+    } else {
+      if (el.classList.contains("fout")) el.classList.remove("fout");
+    }
+  }
+  checkAanvInf(index: number, aanvInf: string, el: any): void {
+    if (aanvInf != "" && aanvInf != this.selectedWoorden[this.huidigWoordIndex].aanvInf[index].term) {
+      //el.focus();
+      el.select();  //el.setSelectionRange(0,el.value.length); in sommige browsers 
+      if (!el.classList.contains("fout")) el.classList.add("fout");
+      console.log(el.parentNode.className);
+      this.aantFout += 1;
+    } else {
+      if (el.classList.contains("fout")) el.classList.remove("fout");
+    }
   }
 
   back(): void {
